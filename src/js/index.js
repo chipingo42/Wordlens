cb46813e59446dde425e9503cd99959a398499ca
victor conicy <akalazuvictor42@gmail.com>
2022-03-01 11:46:50 +0000

    new commit

--- src/js/index.js
+++ src/js/index.js
@@ -11,34 +11,20 @@ darkMode.onclick = function() {
 
 
 
+document.addEventListener('readystatechange', (e) => {
+   if (e.target.readyState === "complete") {
+       initApp()
+   }
+})
 
 
 
+// const initApp = () => {
+//     const search = document.getElementById('Search');
+//     search.addEventListener('input', showClearTextButton);
 
-
-
-
-
-
-
-
-
-
-// function searchShow(query)  {
-//     const basaUrl  = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`
-//      fetch(basaUrl)
-//     .then(res => res.json())
-//     .then((data)  =>  {
-//        const result = data.map(element => element?.word)
-//         console.log(data)
-//         // console.log(result)
-//         renderResults(result)
-
-//         // print the header
-//         // const list = document.getElementById('header').innerText = result.value;
-//         // console.log(list)
-//     }) 
-
+//     const form = document.getElementById('formSearch');
+//     form.addEventListener('submit', submitTheSearch);
 // }
 
 
@@ -46,39 +32,64 @@ darkMode.onclick = function() {
 
 
 
-// function renderResults(result) {
-//     const list = document.getElementById('header');
-//     list.innerText = '';
-//     result.forEach(result => {
 
-//         const element = document.createElement('ul')
 
-//         element.innerText = result;
 
-//         list.appendChild(element);
+function searchShow(query)  {
+    const basaUrl  = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`
+     fetch(basaUrl)
+    .then(res => res.json())
+    .then((data)  =>  {
+       const result = data.map(element => element?.word)
+        console.log(data)
+        // console.log(result)
+        renderResults(result)
+
+        // print the header
+        // const list = document.getElementById('header').innerText = result.value;
+        // console.log(list)
+    }) 
+
+}
+
+
+
+
+
+
+function renderResults(result) {
+    const list = document.getElementById('header');
+    list.innerText = '';
+    result.forEach(result => {
+
+        const element = document.createElement('ul')
+
+        element.innerText = result;
+
+        list.appendChild(element);
         
-//     });
-// }
+    });
+}
  
 
-// let searchTimeOut = 0;
+let searchTimeOut = 0;
 
-// window.onload = () => {
-//     const searchFeildElement = document.getElementById('Search')
-//     searchFeildElement.onkeyup = (even) => {
-//         // console.log(searchFeildElement.value)  
+window.onload = () => {
+    const searchFeildElement = document.getElementById('Search')
+    searchFeildElement.onkeyup = (even) => {
+        // console.log(searchFeildElement.value)  
         
-//         clearTimeout( searchTimeOut);
+        clearTimeout( searchTimeOut);
         
-//         if(searchFeildElement.value.trim().length === 0) {
-//             return;
-//         }
+        if(searchFeildElement.value.trim().length === 0) {
+            return;
+        }
 
 
-//         searchTimeOut = setTimeout(() => {
-//                searchShow(searchFeildElement.value)
-//         }, 250);
+        searchTimeOut = setTimeout(() => {
+               searchShow(searchFeildElement.value)
+        }, 250);
          
-//     }
-// }
+    }
+}
 
