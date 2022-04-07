2cdaebf62db4a165b086ba0343582e155ac3a2ae
victor conicy <akalazuvictor42@gmail.com>
2022-04-07 10:31:53 +0100

    new update

--- src/js/index.js
+++ src/js/index.js
@@ -28,26 +28,6 @@ function fetchWord(word) {
         console.log(data)
         
     })
-
-    // try {
-    //     let responese = await fetch(
-    //         "https://api.dictionaryapi.dev/api/v2/entries/en/" + word,
-    //     )
-
-    //     if (respone.ok == false) {
-    //         // skeleton.style.display = 'none'
-    //         // error.style.display = 'block'
-    //         // searchResult.style.display = 'none'
-    //         // meaningDiv.style.display = 'none'
-    //         return false
-    //     } else {
-    //         // skeleton.style.display = 'none'
-    //         // error.style.display = 'none'
-    //         // searchResult.style.display = 'block'
-    //         // meaningDiv.style.display = 'block'
-    //         return await responese.json()
-    //     }
-    // }catch (error) {}
 };
 
 
@@ -75,137 +55,11 @@ function loadListener() {
             return
         }
 
-
-        
-        // let getPhoneticText = data[0]?.phonetics.find((item) => {
-        //     if (item.text?.length > 0) return true
-        // });
-
-        // let getPhoneticAudio = data[0]?.phonetics.find((item) => {
-        //     if (item.audio?.length > 0) return true
-        // });
-        
-
-        // if(getPhoneticAudio !=undefined){
-        //     sound.setAttribute('src', getPhoneticAudio?.audio)
-        //     audiobtn.style.display = "visible"
-        // } else{  
-        // audiobtn.style.hove ="none"
-        // };
-
-        // phoneticText.textContent = getPhoneticText?.text;
+        console.log(data)
         word.textContent = searchBox.value;
+
     })
 }
-
-// async function handleSubmit(e) {
-//     e.preventDefault()
-
-//     if (searchBox.value.length <= 1) {
-//         errorLabel.style.display = 'flex'
-//         return
-//         } else {
-//         errorLabel.style.display = 'none'
-//     };
-
-
-//     const data = await fetchWord(searchBox.value)
-//     if (data == false) {
-//         return
-//     };
-
-
-//     let getPhoneticText = data[0]?.phonetics.find((item) => {
-//         if (item.text?.length > 0) return true
-//     });
-
-//     let getPhoneticAudio = data[0]?.phonetics.find((item) => {
-//         if (item.audio?.length > 0) return true
-//     });
-    
-
-//     if(getPhoneticAudio !=undefined){
-//         sound.setAttribute('src', getPhoneticAudio?.audio)
-//         audiobtn.style.display = "visible"
-//     } else{  
-//      audiobtn.style.hove ="none"
-//     };
-
-//     word.textContent = searchBox.value;
-//     phoneticText.textContent = getPhoneticText?.text;
-
-
-//     let meaningsString = ''
-//     data[0]?.meanings.map((item) => {
-//         meaningsString += meaningsHtml(item)
-//     })
-//     meaningDiv.innerHTML = meaningsString;
-
-//     // console.log(data)
-// };
-
-
-// function meaningsHtml(meaning) {
-//     let others = ' '
-//     meaning.definitions?.map((item) => (others += othersHtml(item)))
-//     let html = `<div class="block">
-//                    <header>${meaning.partOfSpeech}</header>
-//                    <ul class=" list-decimal text-sm md:text-base px-5">
-//                    ${others}
-//                    </ul>
-//                    </div>`
-  
-//     html = html.trim()
-//     return html
-// };
-
-  
-
-
-// function meaningsHtml(meaning) {
-//     let others = '  '
-//     meaning.definitons?.map((item) => (others += otherHtml(item)))
-//     let html = `<div class="originText">
-//                      <header>${meaning.partOfSpeech}</header>
-//                      <ul class="inn">
-//                       ${others}
-//                       </ul>
-//                      </div>`
-
-//     html = html.trim()
-//     return html        
-//     // console.log(html)
-// }
-
-
-
-
-// function otherHtml(definition) {
-//     definitionHtml = `<li class="meaning"> ${definition?.definition}<ul>`
-//     let exampleHtml = ''
-//     let synonymsHtml = ''
-//     // let antonymsHtml = ''
-  
-//     if (definition.example != undefined) {
-//       exampleHtml += `<li class="sentence">sentence: “${definition?.example}"</li>`
-//     }
-//     if (definition.synonyms != undefined && definition.synonyms.length > 0) {
-//       synonymsHtml += `<li class="Synonyms">Synonyms: “${flatArray(
-//         definition?.synonyms,
-//       )}”</li>`
-//     }
-  
-  
-//     definitionHtml += exampleHtml + synonymsHtml  + '</ul></li>'
-//     // return definitionHtml
-//     console.log(definitionHtml)
-// }
-
-// function flatArray(arr) {
-//     return arr.reduce((pv, cv) => {
-//       return pv + ', ' + cv
-//     })
-// }
 
 
 
