23c29c6ee748e929ea90a9a0ced44ac4203ebf6e
victor conicy <akalazuvictor42@gmail.com>
2022-04-09 12:15:25 +0100

    new commit

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,22 +1,14 @@
+const darkMode = document.getElementById('darkIcon');
 const form = document.getElementById('formSend');
 const inputForm = document.getElementById('Search');
 const word = document.getElementById('word');
-const result = document.querySelector('.meanings')
+const phoneticText = document.getElementById('phoneticsName');
 
-// const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-
-
-function fetchWord(word) {
-    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
-    .then((res) => {
-        return res.json()
-    })
-    .then((result) => {
-        data(result)
-    })
-}
+const resultDiv = document.querySelector('.meanings');
 
 
+
+// Event Listners
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -25,6 +17,98 @@ form.addEventListener('submit', (e) => {
     if (data === false) {
         return true
     }
+
+    // let getPhoneticText = data[0]?.phonetics.find((item) => {
+    //     if (item.text?.length > 0) return true
+    // })
+    
+    
+    word.textContent = inputForm.value;
+    // phoneticText.textContent = getPhoneticText?.text
+
 })
+
+
+
+
+function fetchWord(word) {
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+    .then((res) => {
+        return res.json() // converted to object
+    })
+    .then((resultDta) => {
+        data(resultDta)
+    })
+}
+
+function data(resultDta) {
+    console.log(resultDta)
+
+    let tableData = '';
+
+    resultDta[0]?.meanings.map(function(values) {
+        tableData = meaningHtml(values)
+    })
+    resultDiv.innerHTML = tableData;
+}
+
+
+function meaningHtml(meaning) {
+   let everyThings = ' ';
+
+   meaning.definitions?.map((values) => (everyThings += othersHtml(values)))
+
+    let html = `
+        <div class="originText">
+                <h4>${meaning.partOfSpeech}</h4>
+                <ul>${everyThings}</ul>
+        </div>`;
+    html = html.trim();
+    return html    
+}
+
+
+function othersHtml(definition) {
+    definitionHTML =  `<ul>
+                            <li class="meaning">${definition?.definition} </li>
+    </ul>`
+    exampleHTML = '';
+    synonymsHTML = '';
+
+    if (definition.example != undefined) {
+        exampleHTML +=  `<ul>
+                   <li class="sentence">sentence: “${definition?.example}"</li>
+        </ul>`;
+    }
+
+    if (definition.synonyms != undefined && definition.synonyms.length > 0) {
+        synonymsHTML += `<li class="font-medium">Synonyms: “${flatArray(
+          definition?.synonyms,
+        )}”</li>`
+    }
+    
+
+    definitionHTML +=  exampleHTML +  synonymsHTML +'</ul></li>'
+    return definitionHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+darkMode.onclick = function() {
+    document.body.classList.toggle("darkMode")
+    document.body.style.transition = '0.2s'
+};
+
 
 
